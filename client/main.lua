24fb01d5d15268299c8470a6eae12e8e4ad71fe9
Pineapple <148965472+KyffinRichmond@users.noreply.github.com>
2023-11-13 13:39:46 +0000

    Update main.lua
    BJCore.Functions.

--- client/main.lua
+++ client/main.lua
@@ -38,21 +38,6 @@ local function hideLastHint()
 	exports['core']:HideText()
 end
 
-local function DrawText3D(x, y, z, text)
-	SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    BeginTextCommandDisplayText("STRING")
-    SetTextCentre(true)
-    AddTextComponentSubstringPlayerName(text)
-    SetDrawOrigin(x,y,z, 0)
-    EndTextCommandDisplayText(0.0, 0.0)
-    local factor = (string.len(text)) / 370
-    DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
-    ClearDrawOrigin()
-end
-
 -- Ped spawn and mission accept
 CreateThread(function()
 	while true do
@@ -72,7 +57,7 @@ CreateThread(function()
 				TaskStartScenarioInPlace(dealer, "WORLD_HUMAN_AA_SMOKE", 0, false)
 			end
 			if dist <= 2.0 then
-				DrawText3D(Config.MissionMarker.x, Config.MissionMarker.y, Config.MissionMarker.z, "~b~[E]~w~ To accept mission")
+				BJCore.Functions.DrawText3D(Config.MissionMarker.x, Config.MissionMarker.y, Config.MissionMarker.z, "~b~[E]~w~ To accept mission")
 				if IsControlJustPressed(0, 38) then
 					TriggerServerEvent("AttackTransport:akceptujto")
 					sleep = 500
